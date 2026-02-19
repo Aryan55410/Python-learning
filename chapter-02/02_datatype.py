6a2d07755867fad74e2495585ff98a25ee795ee5
Patel Aryan Mahendrabhai <paryan9094@gmail.com>
2026-02-19 21:41:01 +0530

    Add files via upload

---
+++ chapter-02/02_datatype.py
@@ -0,0 +1,10 @@
+a = 1 #a is an integer
+
+
+b = 2.5 #b is a float
+
+c = "aryan" # c is a string
+
+d = False # d is boolean variable
+
+e = None # e is a none variable
---
+++ chapter-02/04_operators.py
@@ -0,0 +1,33 @@
+# arithmatic operators
+a = 6
+b = 9
+c = a + b
+print(c)
+
+# assignment operators
+a = 4-2
+print(a)
+b = 6
+b += 3
+print(b)
+
+
+# comparison operators
+a = 5>5
+print(a)
+
+# logical operators
+
+# truth table of 'or'
+print("True or True =", True or True)
+print("True or False =", True or False)
+print("False or True =", False or True)
+print("False or False =", False or False)
+
+
+# truth table of 'and'
+
+print("True and True =", True and  True)
+print("True and  False =", True and  False)
+print("False and  True =", False and  True)
+print("False and  False =", False and  False)
