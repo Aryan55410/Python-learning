092df2c7add7786e3251f704d8a61b5981159782
Patel Aryan Mahendrabhai <paryan9094@gmail.com>
2026-02-22 22:19:28 +0530

    Add files via upload

---
+++ chapter-05/01_dict.py
@@ -0,0 +1,8 @@
+marks = {
+    "Harry":100,
+    "Aryan":100,
+    "Rohan":25
+}
+
+#print(marks,type(marks))
+print(marks["Harry"])
---
+++ chapter-05/02_dict_method.py
@@ -0,0 +1,10 @@
+marks = {
+    "Harry":100,
+    "Aryan":100,
+    "Rohan":25
+}
+
+
+
+
+print(marks.get("Harry2"))
