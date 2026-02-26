6746f409efd490292c90c246cde291217bd2b0b6
Patel Aryan Mahendrabhai <paryan9094@gmail.com>
2026-02-26 22:41:38 +0530

    Add files via upload

---
+++ chapter-09/practice-01.py
@@ -0,0 +1,19 @@
+import random
+def game():
+    print("Welcome to the Number Guessing Game!")
+    score = random.randint(1, 100)
+    with open("high_score.txt") as file:
+      high_score = file.read()
+      if(high_score!=""):
+        high_score = int(high_score)
+      else:
+        high_score = 0  
+
+    print(f"your score: {score}")
+    if(score>high_score):
+       
+       with open("high_score.txt","w") as file:
+          file.write(str(score))
+       return score
+
+game()
